a58c216130aa7f4deae306e188d40a088f5d29c6
Jose Valerio <contact@josevalerio.com>
2025-10-23 02:56:39 -0400

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use crossbeam_channel::Sender;
 use serde;
 use serde::Serialize;
+use serde_json::{Value, json};
+use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::error::TrySendError;
 
 const DEFAULT_BUFFER_SIZE: usize = 1024;
 
@@ -29,15 +31,16 @@ impl Default for LoggerOptions {
     }
 }
 
+#[derive(Serialize)]
 struct LogObject {
     log_level: LogLevels,
-    message: String,
+    message: Value,
 }
 impl Default for LogObject {
     fn default() -> Self {
         Self {
             log_level: LogLevels::Info,
-            message: String::from("not set"),
+            message: serde_json::to_value(String::from("not set")).unwrap(),
         }
     }
 }
@@ -46,12 +49,39 @@ pub struct Logger {
 }
 
 impl Logger {
-    pub fn new(buffer_size: Option<usize>) -> Logger {
-        // Create the channel
-        let (sender, receiver) =
-            crossbeam_channel::bounded::<LogObject>(buffer_size.unwrap_or_default());
+    // Create the channel
+    pub fn new(options: Option<LoggerOptions>) -> Logger {
+        let options = options.unwrap_or_default();
+        let buffer_size = options.buffer_size.unwrap_or_default();
+        let (sender, mut receiver) = tokio::sync::mpsc::channel::<LogObject>(buffer_size);
 
+        tokio::spawn(async move {
+            loop {
+                match receiver.recv().await {
+                    Some(log) => {
+                        let x = json!({"level": log.log_level, "message": log.log_level });
+                        println!("{}", x)
+                    }
+                    None => {}
+                }
+            }
+        });
         Logger { sender }
+    }
+
+    pub fn send<T: Serialize>(self: Self, value: serde_json::Value) {
+        let x = LogObject {
+            log_level: LogLevels::Info,
+            message: value,
+        };
+
+        match self.sender.try_send(x) {
+            Ok(_) => {}
+            Err(e) => match e {
+                TrySendError::Full(_) => eprintln!("CHANNEL IS FULL"),
+                TrySendError::Closed(_) => eprintln!("CHANNEL CLOSED"),
+            },
+        }
     }
 }
 
