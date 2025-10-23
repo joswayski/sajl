01daf170fb82f3c410ecfec126ad22739c1633b4
Jose Valerio <contact@josevalerio.com>
2025-10-23 04:13:59 -0400

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
-use std::vec;
+use std::io::{Write, stderr};
 
 use serde;
 use serde::Serialize;
 use serde_json::{Value, json};
-use tokio::sync::mpsc::Sender;
-use tokio::sync::mpsc::error::TrySendError;
+use tokio::sync::mpsc::{Sender, error::TrySendError};
+use tokio::time::{self, timeout};
 
+// Size of the channel - max messages you can send at one time
 const DEFAULT_BUFFER_SIZE: usize = 1024;
+
+// How many log messages to batch
+const BATCH_SIZE: usize = 50;
+
+// How long to batch or
+const BATCH_DURATION_MS: u64 = 50;
 
 #[derive(Serialize)]
 #[serde(rename_all = "UPPERCASE")]
@@ -17,18 +24,30 @@ enum LogLevels {
     Debug,
 }
 
-struct LoggerOptions {
+pub struct LoggerOptions {
     /// How many messages to send down the channel before
     /// messages start to be dropped.
     ///
     /// Deault is [`DEFAULT_BUFFER_SIZE`] - 1024
     buffer_size: Option<usize>,
+
+    /// How many log messages to batch
+    ///
+    /// Default is [`BATCH_SIZE`] - 50
+    batch_size: Option<usize>,
+
+    /// For how long to batch messages for
+    ///
+    /// Default is [`BATCH_DURATION_MS`]
+    batch_duration_ms: Option<u64>,
 }
 
 impl Default for LoggerOptions {
     fn default() -> Self {
         Self {
             buffer_size: Some(DEFAULT_BUFFER_SIZE),
+            batch_size: Some(BATCH_SIZE),
+            batch_duration_ms: Some(BATCH_DURATION_MS),
         }
     }
 }
@@ -51,27 +70,48 @@ pub struct Logger {
 }
 
 impl Logger {
-    // Create the channel
     pub fn new(options: Option<LoggerOptions>) -> Logger {
         let options = options.unwrap_or_default();
         let buffer_size = options.buffer_size.unwrap_or_default();
+        let batch_size = options.batch_size.unwrap_or_default();
+        let batch_duration = options.batch_duration_ms.unwrap_or_default();
         let (sender, mut receiver) = tokio::sync::mpsc::channel::<LogObject>(buffer_size);
 
         tokio::spawn(async move {
+            let mut batch = Vec::<LogObject>::with_capacity(batch_size);
+
             loop {
-                match receiver.recv().await {
-                    Some(log) => {
-                        let x = json!({"level": log.log_level, "message": log.log_level });
-                        println!("{}", x)
+                match timeout(time::Duration::from_millis(batch_duration), receiver.recv()).await {
+                    Ok(Some(log)) => {
+                        // Found a log!
+                        batch.push(log);
+                        if batch.len() >= batch_size {
+                            flush(&batch);
+                            batch.clear();
+                        }
                     }
-                    None => {}
+                    Ok(None) => {
+                        // Channel closed, flush before dropping
+                        if !batch.is_empty() {
+                            flush(&batch);
+                        }
+                        break;
+                    }
+                    Err(_) => {
+                        // timeout received
+                        if !batch.is_empty() {
+                            flush(&batch);
+                            batch.clear();
+                        }
+                    }
                 }
             }
         });
+
         Logger { sender }
     }
 
-    pub fn send<T: Serialize>(self: &Self, data: &T) {
+    fn send<T: Serialize>(self: &Self, data: &T) {
         let value = match serde_json::to_value(data) {
             Ok(v) => v,
             Err(e) => {
@@ -95,59 +135,15 @@ impl Logger {
     }
 }
 
-#[derive(Serialize, Debug)]
-enum Items {
-    Apple,
-    Ipod,
-    Puter,
-    Steak,
-}
-
-#[derive(Serialize, Debug)]
-enum ToyotaModel {
-    Rav3,
-    Camry,
-}
-
-#[derive(Serialize, Debug)]
-enum TeslaModel {
-    Model3,
-    ModelS,
-}
-
-#[derive(Serialize, Debug)]
-enum Car {
-    Toyota(ToyotaModel),
-    Tesla(TeslaModel),
-}
-
-#[derive(Serialize, Debug)]
-struct Child {
-    toy: String,
-    age: usize,
-}
-#[derive(Serialize, Debug)]
-struct User {
-    name: String,
-    age: usize,
-    items: Vec<Items>,
-    children: Vec<Child>,
-    car: Car,
-}
-fn main() {
-    let logger = Logger::new(None);
-
-    let user = User {
-        name: "Jose".to_string(),
-        age: 43,
-        items: vec![Items::Ipod, Items::Steak],
-        car: Car::Tesla(TeslaModel::ModelS),
-        children: vec![Child {
-            age: 12,
-            toy: "beans".to_string(),
-        }],
-    };
-    logger.send(&user);
-
-    println!("USER outside logger {:#?}", &user)
+fn flush(batch: &Vec<LogObject>) {
+    // Lock once for the whole batch
+    let mut stderr = stderr().lock();
+    for log in batch {
+        writeln!(
+            stderr,
+            "{}",
+            json!({"level": log.log_level, "data": log.data })
+        )
+        .ok();
+    }
 }
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use sajl::Logger;
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+enum Items {
+    Apple,
+    Ipod,
+    Puter,
+    Steak,
+}
+
+#[derive(Serialize, Debug)]
+enum ToyotaModel {
+    Rav3,
+    Camry,
+}
+
+#[derive(Serialize, Debug)]
+enum TeslaModel {
+    Model3,
+    ModelS,
+}
+
+#[derive(Serialize, Debug)]
+enum Car {
+    Toyota(ToyotaModel),
+    Tesla(TeslaModel),
+}
+
+#[derive(Serialize, Debug)]
+struct Child {
+    toy: String,
+    age: usize,
+}
+#[derive(Serialize, Debug)]
+struct User {
+    name: String,
+    age: usize,
+    items: Vec<Items>,
+    children: Vec<Child>,
+    car: Car,
+}
+
+#[tokio::main]
+async fn main() {
+    let logger = Logger::new(None);
+
+    let user = User {
+        name: "Jose".to_string(),
+        age: 43,
+        items: vec![Items::Ipod, Items::Steak],
+        car: Car::Tesla(TeslaModel::ModelS),
+        children: vec![Child {
+            age: 12,
+            toy: "beans".to_string(),
+        }],
+    };
+    logger.send(&user);
+
+    println!("USER outside logger {:#?}", &user)
+}
